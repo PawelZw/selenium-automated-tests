2738a525e4e43ad7280ef8771ce7c9aaa65adedf
PawelZw <nazwa_uzytkownika@users.noreply.github.com>
2023-02-07 23:02:42 +0100

    hover tests 1.2

--- src/test/java/militaria/tests/HoverTests.java
+++ src/test/java/militaria/tests/HoverTests.java
@@ -11,13 +11,132 @@ import org.testng.annotations.Test;
 public class HoverTests {
 
     @Test
-    public void performAction() {
+    public void performActionBestsellers() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
 
         Actions actions = new Actions(driver);
         WebElement heading = driver.findElement(By.linkText("Bestsellery"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionMensGifts() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Męskie Prezenty"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionNews() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Nowości"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionPromotions() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Promocje"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionGraver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Męskie Prezenty"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionGiftCards() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Karty podarunkowe"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionLoyaltyClubPromotions() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Promocje KSK"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+
+    @Test
+    public void performActionShooting() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Strzelectwo"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionSelfDefense() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Samoobrona"));
+        actions.moveToElement(heading).perform();
+
+    }
+
+    @Test
+    public void performActionOutdoor() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.militaria.pl/");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.linkText("Outdoor"));
         actions.moveToElement(heading).perform();
 
     }

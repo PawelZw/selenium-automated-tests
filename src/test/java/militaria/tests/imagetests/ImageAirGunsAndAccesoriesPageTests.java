38426c9e9986c913385b8552a0d9471372cd16b4
PawelZw <nazwa_uzytkownika@users.noreply.github.com>
2023-03-22 15:54:48 +0100

    image assertion Air gun accesories page test

--- src/test/java/militaria/tests/imagetests/ImageAirGunsAndAccesoriesPageTests.java
+++ src/test/java/militaria/tests/imagetests/ImageAirGunsAndAccesoriesPageTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,32 +13,50 @@ import java.io.IOException;
 
 public class ImageAirGunsAndAccesoriesPageTests {
 
-    /** The class contains image upload assertions for the Air Guns and Accesories page https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml **/
+    /** The class contains image upload assertions for the Air Gun Accesories page https://militaria.pl/c/wiatrowki-i-akcesoria-7742 **/
 
     @Test
     public void imageAirGunCarabinersUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[1]/li[1]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[2]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
 
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imagePistolsRevolversUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[1]/li[2]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[3]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "55");
+        Assert.assertEquals(height, "133");
+        driver.quit();
+    }
+    @Test
+    public void imageAirGunSerialShootingUploadTest() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
+
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[4]/div/div/a/img"));
+        String height = image.getAttribute("height");
+        System.out.println(height);
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
@@ -45,104 +64,120 @@ public class ImageAirGunsAndAccesoriesPageTests {
     @Test
     public void imageAirGunGasSpringUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[1]/li[4]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[5]/div/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "55");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAirGunPCPUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[2]/li[1]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[6]/div/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAirGunPCAUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[2]/li[2]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[7]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAirGunWithEngraverUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[2]/li[3]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[8]/div/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAirGunProducersUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[2]/li[4]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[9]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAirGunTo500UploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[3]/li[1]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[10]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageBuckshotUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[3]/li[2]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[11]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "36");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
     @Test
     public void imageAccessoriesUploadTest() throws IOException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.militaria.pl/wiatrowki_i_akcesoria_c334.xml");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://militaria.pl/c/wiatrowki-i-akcesoria-7742");
 
-        WebElement image = driver.findElement(By.xpath("//*[@id=\"category-main\"]/div/div[3]/div[1]/ul[3]/li[3]/div/a/img"));
+        WebElement image = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/ul/li[12]/div[1]/div/a/img"));
         String height = image.getAttribute("height");
         System.out.println(height);
-        Assert.assertEquals(height, "55");
+        Assert.assertEquals(height, "133");
         driver.quit();
     }
 
